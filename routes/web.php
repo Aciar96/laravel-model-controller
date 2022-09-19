8d8b9ce418ae7db79996d22dbe50e3aa058692fc
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-09-19 18:00:55 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
+Route::get('/comicx', 'HomeController@index')->name('movies');
+
